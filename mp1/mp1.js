5e3f0e46300dbe1dbe3de4d965ae88357e1e64c4
James Timotiwu <architect.idol5@gmail.com>
2020-02-02 14:39:27 -0600

    Working I

--- mp1/mp1.js
+++ mp1/mp1.js
@@ -7,13 +7,19 @@
 /** @global WebGL context */
 var gl;
 
+/** @global Shader program */
+var shaderProgram;
+
+/** @global Position buffer */
+var positionBuffer;
+
 /**
  * Get WebGL context for canvas
  * @param {element} canvas canvas
  * @return {Object} WebGL context
  */
 function getGLContext(canvas) {
-     var context = getContext("webgl");
+     var context = canvas.getContext("webgl");
 
      if (context) {
          context.viewportWidth = canvas.width;
@@ -27,7 +33,7 @@ function getGLContext(canvas) {
      return context;
 }
 
-/**
+/** 
  * Transformations:
  * Scale
  * Rotation
@@ -39,8 +45,17 @@ function updateTransforms() {
     var rotation = [0, 1]
 }
 
-function updateColor() {
-    var color = [0, 0, 0, 1]
+function setColor() {
+  var r1;
+  var g1;
+  var b1;
+
+  var color = [r1, g1, b1, 1];
+  var colorUniformLocation = gl.getUniformLocation(program, "u_color");
+  gl.uniform4f(colorUniformLocation, r1, g1, b1, 1)
+  gl.bufferData(gl.ARRAY_BUFFER,
+    new Float32Array(color),
+    gl.STATIC_DRAW);
 }
 
 /**
@@ -90,14 +105,59 @@ function initializeGeometry() {
     // Add vertices into buffer
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
   }
-console.log(positions)
 
-function initalizeBuffers() {
-  var positionBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, positiionBuffer)
+/**
+ * Draws function with transformation factor on positions
+ */
+function draw(scale, translation) {
+  /**
+  var resolution;
+  var translation;
+  var scale;
+  */
+ // var scale = [1.08, 1.41]
+ // var translation = [100, 150];
+ var positionAttributeLocation = gl.getAttribLocation(shaderProgram, "a_position");
+ 
+ // Tell WebGL how to convert from clip space to pixels
+ gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+ // Clear canvas
+ gl.clear(gl.COLOR_BUFFER_BIT);
+ gl.useProgram(shaderProgram);
+ 
+  // Set window resolution
+  gl.uniform2f(gl.getUniformLocation(shaderProgram, "u_resolution"), gl.canvas.width, gl.canvas.height);
+  gl.enableVertexAttribArray(positionAttributeLocation);
+  // 2 components per iteration
+  gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  // Draw geometry (primitive type, offset, number of points in all triangles)
+  initializeGeometry()
+
+  // Set translation
+  gl.uniform2fv(gl.getUniformLocation(shaderProgram, "u_translation"), translation);
+  // Set scaling magnitude 
+  gl.uniform2fv(gl.getUniformLocation(shaderProgram, "u_scale"), scale);
+  //gl.drawArrays(gl.TRIANGLES, 0, vertexPositionBuffer.numberOfItems);
+  // Set color
+  gl.uniform4f(gl.getUniformLocation(shaderProgram, "u_color"), 0, 1, 1, 1)
+
+  gl.drawArrays(gl.TRIANGLES, 0, 42)
 }
 
-function initalizeShaders() {
+/**
+ * Initialize buffer for vertex position
+ */
+function initializeBuffers() {
+  positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+}
+
+/**
+ * Initializes shader program based on shader code in DOM
+ */
+function initializeShaderProgram() {
   var vertexShader = initializeShader("vertex-shader", gl.VERTEX_SHADER)
   var fragmentShader = initializeShader("fragment-shader", gl.FRAGMENT_SHADER)
 
@@ -108,8 +168,9 @@ function initalizeShaders() {
 
   if (gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
     gl.useProgram(shaderProgram);
-    shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
-    shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor"); 
+    shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "a_position");
+    shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "u_color"); 
+    return shaderProgram;
   }
 
   console.log("initializeShader: Error setting up shaders")
@@ -148,9 +209,11 @@ function startup() {
   var canvas = document.getElementById("mp1GLCanvas")
   gl = getGLContext(canvas)
 
-  initializeShaders();
+  initializeShaderProgram();
   initializeBuffers();
-  initializeGeometry();
+  gl.clearColor(1.0, 1.0, 1.0, 1.0);
+  draw([1.08, 1.41], [110, 150]);
+  draw([1.50, 1.41], [100, 150]);
 
   /**
    * 1. Draw geometry for Illini
@@ -162,6 +225,14 @@ function startup() {
    * 4. Animate using affine transformations (update geometry for every frame)
    */
 
-  gl.clearColor(0.0, 0.0, 0.0, 0.0) // Specifies color for clear canvas
+   /* const numInstances = 2;
+   const matricies = [
+     m4.identity(),
+     m4.identity()
+   ]
 
+   const colors = [
+     [1, 0, 1, 1
+    [1, 1, 0, 1]]
+   ] */
 }
