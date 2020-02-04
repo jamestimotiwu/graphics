6a8c30ef31f571a267ff41cd0cd8ab951f22c6fb
James Timotiwu <architect.idol5@gmail.com>
2020-02-03 21:51:27 -0600

    Removed unneccessary variables in shader program

--- mp1/mp1.js
+++ mp1/mp1.js
@@ -3,7 +3,6 @@
  * @author James Timotiwu <jit2@illinois.edu>
  */
 
-
 /** @global WebGL context */
 var gl;
 
@@ -48,19 +47,6 @@ function updateTransforms() {
     var rotation = [0, 1]
 }
 
-function setColor() {
-  var r1;
-  var g1;
-  var b1;
-
-  var color = [r1, g1, b1, 1];
-  var colorUniformLocation = gl.getUniformLocation(program, "u_color");
-  gl.uniform4f(colorUniformLocation, r1, g1, b1, 1)
-  gl.bufferData(gl.ARRAY_BUFFER,
-    new Float32Array(color),
-    gl.STATIC_DRAW);
-}
-
 /**
  * Set up Illini mesh, vertices, edges
  */
@@ -68,20 +54,14 @@ function initializeGeometry() {
     /**
      * Set up adjustable offsets for the middle rectangle in the I figure
      */
-    /* 
-    var left_offset = 10 + I_rect_width;
-    var right_offset = 100 - I_rect_height;
-    var top_offset = 10 + I_rect_height;
-    var bottom_offset = 100 - I_rect_height;
-    var border_size = 0.01; */
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
+    var border_size = 0.045;
     var base_height = 0.25;
-    var base_width = 0.75;
-    var column_height = 1.0;
-    var column_width = 0.4;
+    var base_width = 0.72;
+    var column_height = 1.12;
+    var column_width = 0.35;
 
-    // Translate to clip
+    // Translate to clip in the middle
     var translated_base_h = base_height - base_height / 2.0;
     var translated_base_w = base_width - base_width / 2.0;
     var translated_column_h = column_height - column_height / 2.0;
@@ -105,7 +85,25 @@ function initializeGeometry() {
       [translated_base_w, -translated_column_h]
     ]
 
-    var edge_list = [
+    var vertex_list_border = [
+      // Top base rectangle
+      [-translated_base_w-border_size, translated_column_h+border_size],
+      [translated_base_w+border_size, translated_column_h+border_size],
+      [-translated_base_w-border_size, translated_column_h - base_height - border_size],
+      [translated_base_w+border_size, translated_column_h - base_height - border_size],
+      // Column rectangle
+      [-translated_column_w-border_size, translated_column_h],
+      [translated_column_w+border_size, translated_column_h],
+      [-translated_column_w-border_size, -translated_column_h],
+      [translated_column_w+border_size, -translated_column_h],
+      // Bottom base rectangle
+      [-translated_base_w-border_size, -translated_column_h + base_height + border_size],
+      [translated_base_w+border_size, -translated_column_h + base_height + border_size],
+      [-translated_base_w-border_size, -translated_column_h - border_size],
+      [translated_base_w+border_size, -translated_column_h - border_size]
+    ]
+
+    var triangles = [
       // Top rectangle for border
       [1, 2, 3],
       [3, 2, 4],
@@ -116,39 +114,23 @@ function initializeGeometry() {
       [9, 10, 11],
       [12, 10, 11]
     ]
-/*     var vertex_list = [
-      // Top rectangle for border
-      [-0.5, 0.5],WWW
-      [0.5, 0.5],
-      [-0.5, 0.3],
-      [0.5, 0.3],
-      // Middle rectangle for border
-      [-0.3, 0.5],
-      [0.3, 0.5],
-      [-0.3, -0.5],
-      [0.3, -0.5],
-      // Bottom rectangle for border
-      [-0.5, -0.3],
-      [0.5, -0.3],
-      [-0.5, -0.5],
-      [0.5, -0.5],
-      // Top rectangle
 
-      // Middle rectangle
-      
-      // Bottom rectangle
-    ]
-
-    var vertex_list_border = [
-
-    ]
-    */
     var positions = [];
     // Set positions based on edges and verticies
-    for (var i = 0; i < edge_list.length; i++) {
+
+    // Border rectangles
+    for (var i = 0; i < triangles.length; i++) {
       for (var j = 0; j < 3; j++) {
-        positions.push(vertex_list[[edge_list[i][j] - 1]][0]);
-        positions.push(vertex_list[[edge_list[i][j] - 1]][1]);
+        positions.push(vertex_list_border[[triangles[i][j] - 1]][0]);
+        positions.push(vertex_list_border[[triangles[i][j] - 1]][1]);
+      }
+    }
+
+    // Shape rectangles
+    for (var i = 0; i < triangles.length; i++) {
+      for (var j = 0; j < 3; j++) {
+        positions.push(vertex_list[[triangles[i][j] - 1]][0]);
+        positions.push(vertex_list[[triangles[i][j] - 1]][1]);
       }
     }
     
@@ -156,42 +138,41 @@ function initializeGeometry() {
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 }
 
-function setColor() {
+/**
+ * Set up Illini colors for border and shape
+ */
+function initializeColor() {
   gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer)
-  var color = [0.07, 0.16, 0.295, 1]
+  var colorBorder = [0.07, 0.16, 0.295, 1]
+  var colorShape = [0.909, 0.29, 0.15, 1]
   var colors = [];
+  // 6 triangles
   for (var i = 0; i < 18; i++) {
+    // 3 vertices per triangle
+      // r, g, b, l
     for (var j = 0; j < 4; j++) {
-      colors.push(color[j]);
+      colors.push(colorBorder[j]);
     }
   }
+
+  // 6 triangles
+  for (var i = 0; i < 18; i++) {
+      // r, g, b, l
+    for (var j = 0; j < 4; j++) {
+      colors.push(colorShape[j]);
+    }
+  }
+
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-}
-  
+}  
 
 /**
  * Draws function with transformation factor on positions
  */
 function draw() {
-  /**
-  var resolution;
-  var translation;
-  var scale;
-  */
- // var scale = [1.08, 1.41]
- // var translation = [100, 150];
-  // Draw geometry (primitive type, offset, number of points in all triangles)
   initializeGeometry()
-  setColor();
-
-  // Set translation
-  //gl.uniform2fv(gl.getUniformLocation(shaderProgram, "u_translation"), translation);
-  // Set scaling magnitude 
-  //gl.uniform2fv(gl.getUniformLocation(shaderProgram, "u_scale"), scale);
-  // Set color
-  //gl.uniform4fv(gl.getUniformLocation(shaderProgram, "u_color"), [0.07, 0.16, 0.295, 1])
-
-  gl.drawArrays(gl.TRIANGLES, 0, 18)
+  initializeColor();
+  gl.drawArrays(gl.TRIANGLES, 0, 36)
 }
 
 /**
@@ -202,19 +183,22 @@ function initializeBuffers() {
   var colorAttributeLocation = gl.getAttribLocation(shaderProgram, "a_color");
   positionBuffer = gl.createBuffer();
   colorBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
- // Tell WebGL how to convert from clip space to pixels
+
+   // Tell WebGL how to convert from clip space to pixels
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
   // Clear canvas
   gl.clear(gl.COLOR_BUFFER_BIT);
   gl.useProgram(shaderProgram);
   // Set window resolution
   gl.uniform2f(gl.getUniformLocation(shaderProgram, "u_resolution"), gl.canvas.width, gl.canvas.height);
+  
+  // Enable vertex buffers
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
   gl.enableVertexAttribArray(positionAttributeLocation);
   // 2 components per iteration
   gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
 
-  // Enable color Buffers
+  // Enable color buffers
   gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
   gl.enableVertexAttribArray(colorAttributeLocation);
   gl.vertexAttribPointer(colorAttributeLocation, 4, gl.FLOAT, false, 0, 0);
@@ -280,27 +264,4 @@ function startup() {
   initializeBuffers();
   gl.clearColor(1.0, 1.0, 1.0, 1.0);
   draw();
-  //draw([1.08, 1.41], [0, 0], [0.07, 0.16, 0.295, 1]);
-  //draw([1.0, 1.41], [0, 0], [0.909, 0.29, 0.15, 1]);
-
-  /**
-   * 1. Draw geometry for Illini
-   * 2. Use two triangles overlapped to create the border
-   * 3. Scale the background triangle slightly larger than the first
-   *  1. Set background I to orange
-   *  2. Set foreground I to blue
-   *  3. Maintain origin for triangles
-   * 4. Animate using affine transformations (update geometry for every frame)
-   */
-
-   /* const numInstances = 2;
-   const matricies = [
-     m4.identity(),
-     m4.identity()
-   ]
-
-   const colors = [
-     [1, 0, 1, 1
-    [1, 1, 0, 1]]
-   ] */
 }
